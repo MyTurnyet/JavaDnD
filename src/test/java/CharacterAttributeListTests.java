6c937f5f78cd1ba06089123d845c25c184b63d39
Paige Watson <paige.watson@outlook.com>
2022-03-10 12:43:04 -0800

    updated to Java 11 and junit tests

--- src/test/java/CharacterAttributeListTests.java
+++ src/test/java/CharacterAttributeListTests.java
@@ -1,4 +1,6 @@
-import org.testng.annotations.Test;
+
+
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
--- src/test/java/CharacterTests.java
+++ src/test/java/CharacterTests.java
@@ -1,4 +1,5 @@
-import org.testng.annotations.Test;
+
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
